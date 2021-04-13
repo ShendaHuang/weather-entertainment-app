4ec62337636adcf532bb1c5acbba40bab9f77442
Xingjian Wang <xwang7@villanova.edu>
2021-04-13 18:44:13 +0800

    Designed search page prototype and created a weather description json file

--- App.js
+++ App.js
@@ -16,9 +16,17 @@ const styles = StyleSheet.create({
   
   slide2: {
     flex: 1,
-    backgroundColor:'deepskyblue',
+    backgroundColor:'aqua',
 	alignItems:'center',
 	justifyContent:'center',
+  },
+  
+  slide3: {
+    flex: 1,
+    backgroundColor:'white',
+	//alignItems:'center',
+	//justifyContent:'center',
+	paddingTop:40,
   },
   
   container:{
@@ -27,10 +35,9 @@ const styles = StyleSheet.create({
 	alignItems:'center',
 	justifyContent:'center',
   },
-  
   textInput: {
 	borderColor: '#CCCCCC',
-	color: 'ghostwhite',
+	color: 'black',
 	borderTopWidth: 1,
 	borderBottomWidth: 1,
 	height: 50,
@@ -39,7 +46,6 @@ const styles = StyleSheet.create({
 	paddingLeft: 20,
 	paddingRight: 20,
   },
-  
   saveButton: {
 	borderWidth: 1,
 	borderColor: '#007BFF',
@@ -47,87 +53,111 @@ const styles = StyleSheet.create({
 	width:400,
 	padding: 15,
   	margin: 5,
+	marginBottom: 50
+  },
+  historyButton:{
+	borderWidth: 1,
+	borderColor: '#007BFF',
+	backgroundColor: '#007BFF',
+	width:200,
+	padding: 15,
+  	margin: 5,
 	marginBottom: 100
   },
-  
+  modelButton:{
+    borderWidth: 1,
+	borderColor: '#007BFF',
+	backgroundColor: '#007BFF',
+	width:200,
+	padding: 15,
+  	margin: 5,
+	marginBottom: 50
+  },
   GPSButton:{
 	borderWidth: 1,
 	borderColor: '#007BFF',
 	backgroundColor: 'white',
-	width:400,
+	width:250,
 	padding: 15,
   	margin: 5,  
   },
-  
   GPSText:{
 	color: 'black',
 	fontSize: 30,
 	textAlign: 'center'  
   },
-  
   saveButtonText: {
 	color: '#FFFFFF',
 	fontSize: 20,
 	textAlign: 'center'
   },
-  
   Image:{
 	alignItems:'center',
 	justifyContent:'center',
 	width:230,
 	height:205,
-	marginBottom:50,
+	marginBottom:5,
   },
-  
-  BottomText:{
-	color:'#FFFFFF',
+  UserText:{
+	color:'#007BFF',
 	fontSize: 20,
 	textAlign: 'center',
   },
-  
+  BottomText:{
+	color:'#007BFF',
+	fontSize: 12,
+	textAlign: 'center',
+	marginBottom: 20
+  },
   TopText:{
 	color:'#007BFF',
 	fontSize: 40,
 	textAlign: 'center',
 	marginBottom: 30
-  },
-  
-  ActiveDot:{
-	backgroundColor: '#fff',
-    width: 10,
-    height: 10,
-    borderRadius: 7,
-    marginLeft: 7,
-    marginRight: 7
-  },
-  
-   Dot:{
-	backgroundColor: 'rgba(255,255,255,.3)',
-    width: 10,
-    height: 10,
-    borderRadius: 7,
-    marginLeft: 7,
-    marginRight: 7
-   }
+  }
 });
 
-class HomeScreen extends Component{
+class WeatherScreen extends Component{
+
+	render(){
+		return(
+		<View>
+			<Text style = {{fontSize:30}}>Weather Screen</Text>
+		</View>
+		)
+	}
+}
+
+
+class MusicScreen extends Component {
+
+	render(){
+		return(
+		<View>
+			<Text style = {{fontSize:30}}>Music Screen</Text>
+		</View>
+		)
+	}
+}
+
+
+class SearchScreen extends React.Component{
 	constructor(props) {
 		super(props);
 		this.state = { 
 			name: '',
 			stored_name:'',
-			stored_time:'',
+			search_1:'',
+			search_2:'',
+			search_3:'',
 		}
 	}
 	
 	handleNameChange = text => {this.setState({ name: text });}
 	
 	storeData = async() => {
-	   var timeStamp = new Date().toLocaleString('en-US');
 	   try{
 		   await AsyncStorage.setItem('@stored_name',this.state.name)
-		   await AsyncStorage.setItem('@stored_time',timeStamp)
 	   } catch(e){
 		   alert('Failed to save the name')
 	   }
@@ -136,9 +166,7 @@ class HomeScreen extends Component{
 	getData = async() =>{
 		try{
 			const stored_name = await AsyncStorage.getItem('@stored_name');
-			const stored_time = await AsyncStorage.getItem('@stored_time');
 			this.setState({stored_name:stored_name})
-			this.setState({stored_time:stored_time})
 		}
 		catch(e){alert('Failed to get the name')}
 	}
@@ -147,172 +175,163 @@ class HomeScreen extends Component{
 		await AsyncStorage.clear();
 	}
 	
+	FirstModel = async(data) =>{
+		if(data != null){
+		this.setState({search_1:data})}
+		await this.props.parentCallback(this.state.search_1)
+	}
+	
+	SecondModel = async(data) =>{
+		if(data != null){
+		this.setState({search_2:data})}
+		await this.props.parentCallback(this.state.search_2)
+	}
+	
+	ThirdModel = async(data) =>{
+		if(data != null){
+		this.setState({search_3:data})}
+		await this.props.parentCallback(this.state.search_3)
+	}
+	
 	componentDidMount = async() =>{
-		//await this.clearData();
+		const w = require("./assets/weather_words.json");
+		
+		var Num_1 = Math.floor(Math.random()*45);
+		var weather_1 = w.Weather[Num_1];
+		
+		var Num_2 = Math.floor(Math.random()*45);
+		var weather_2 = w.Weather[Num_2];
+		
+		var Num_3 = Math.floor(Math.random()*45);
+		var weather_3 = w.Weather[Num_3];
+		
+		
+		await this.FirstModel(weather_1);
+		await this.SecondModel(weather_2);
+		await this.ThirdModel(weather_3);
 		await this.getData();
-		await this.props.parentCallback(this.state.stored_name,this.state.stored_time)
+		await this.props.parentCallback(this.state.stored_name)
 	}
 
 	activate = async() =>{
 		await this.storeData();
 		await this.getData();
-		await this.props.parentCallback(this.state.stored_name,this.state.stored_time)
+		await this.props.parentCallback(this.state.stored_name)
+	}
+	
+	search_history = async() =>{
+		await this.props.parentCallback(this.state.stored_name)
 	}
 
 	render(){
 	  return(
-		<View style={styles.slide1}>
-		    <Text style={styles.TopText}> VU GeoFacts App</Text>
+		<View style={styles.slide3}>
 		    <TextInput 
 			  style={styles.textInput}
-			  placeholder="Your name"
-			  placeholderTextColor='ivory'
-			  maxLength={20}s
+			  placeholder="Search"
+			  placeholderTextColor='darkgray'
+			  maxLength={20}
 			  onBlur={Keyboard.dismiss}
 			  onChangeText={this.handleNameChange}
 			  clearButtonMode="always"
+			  returnKeyType="search"
 		    />
 			
 
 			<TouchableOpacity style={styles.saveButton}
 			  onPress={() => {this.activate();}}> 
-			  <Text style={styles.saveButtonText}>Submit Name</Text>
+			  <Text style={styles.saveButtonText}>Search</Text>
 			</TouchableOpacity>
 			
+			<Text style = {{fontSize:20}}> Search History </Text>
+
+			<TouchableOpacity style={styles.historyButton}
+			  onPress={() => {this.search_history()}}> 
+			  <Text style={styles.saveButtonText}>{this.state.stored_name}</Text>
+			</TouchableOpacity>
 			
-            <Image
-              style = {styles.Image}
-              source={require('./img/Villanova.png')}
-            />
+			<Text style = {{fontSize:20}}> Search Example</Text>
 			
-			<Text style={styles.BottomText}>Hello {this.state.stored_name}</Text>
-			<Text style={styles.BottomText}>Last Entered: {this.state.stored_time}</Text>
+			<TouchableOpacity style={styles.modelButton}
+			  onPress={() => {this.FirstModel()}}> 
+			  <Text style={styles.saveButtonText}>{this.state.search_1}</Text>
+			</TouchableOpacity>
+			
+			<TouchableOpacity style={styles.modelButton}
+			  onPress={() => {this.SecondModel()}}> 
+			  <Text style={styles.saveButtonText}>{this.state.search_2}</Text>
+			</TouchableOpacity>
+			
+			<TouchableOpacity style={styles.modelButton}
+			  onPress={() => {this.ThirdModel()}}> 
+			  <Text style={styles.saveButtonText}>{this.state.search_3}</Text>
+			</TouchableOpacity>
+			
 			
          </View>
 	  )
 	}
 }
 
-class GeolocationExample extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-		latitude: null,
-		longitude: null,
-		error: null,
-		building:null,
-		facts:null,
-		gps:null,
-		fact:null,
-		};
-	}
-	
-	activate = async() => {
-		var min = 5;
-		var result_x = 0;
-		
-		const b = require("./assets/gpsbuildings.json");
-		const f = require("./assets/facts.json");
-		
-		navigator.geolocation.getCurrentPosition(
-		(position) => {
-			this.setState({
-				latitude: position.coords.latitude,
-				longitude: position.coords.longitude,
-				error: null,
-			});
-
-			if(this.state.latitude != null){
-				for(var x=0; x<4; x++){
-					var result = Math.sqrt(Math.pow(this.state.latitude-b[x].Latitude , 2) + Math.pow(this.state.longitude-b[x].Longitude , 2));
-					if(result < min){ min = result; result_x = x;}
-				}
-
-				this.setState({ building: b[result_x].Building})
-		
-				var bname = b.[result_x].Building;
-
-				var factNum = Math.floor(Math.random()*5);
-		
-				var factstring = f[bname][factNum];
-				this.setState({ facts:factstring})
-			}
-		},
-		(error) => this.setState({ error: error.message }),
-		{enableHighAccuracy: false, timeout: 5000, maximumAge: Infinity },
-		);
-	}
-	render(){
-	  var lat = this.state.latitude;
-	  var lon = this.state.longitude;
-	  if(lat!=null){
-		var _lat = lat.toFixed(6);
-		var _lon = lon.toFixed(6);
-	  }
-	  final_lat = _lat;
-	  final_lon = _lon;
-	  return(  
-		<View style = {styles.container}>
-			<TouchableOpacity style={styles.GPSButton}
-				onPress={() => {this.activate()}}>
-				<Text style={styles.GPSText}>Tell Me About My Location</Text>  
-			</TouchableOpacity>
-			
-			<Text style = {{fontSize:30}}>Lat: {final_lat}</Text>
-			<Text style = {{fontSize:30}}>Lon: {final_lon}</Text>
-			
-			<Text style = {{fontSize:30}}>Closest Building: {this.state.building}</Text>
-			
-			<Text style = {{fontSize:20, marginBottom:50, marginTop:5, marginRight:10, marginLeft:10}}>Fact: {this.state.facts}</Text>
-
-			<Image source={require('./img/Geo.png')} style={styles.Image}/>
-			
-			<Text style = {{fontSize:20}}>Hello {this.props.Stored_name}</Text>
-			<Text style = {{fontSize:20}}>Last Entered: {this.props.Stored_time}</Text>
-		</View>
-		)
-	}
-}
-
-
 export default class APP extends Component {
   constructor(props) {
 	super(props);
 	this.state = {
+		Name:'',
 		stored_name:'',
-		stored_time:'',
-		lat:null,
-		lon:null
 		}
 	}
-
-	setName = (data_1,data_2) => {
-		this.setState({stored_name:data_1,stored_time:data_2});
-	}
 	
+	setName = (data_1) => {
+		this.setState({stored_name:data_1});
+	}
+
   render() {
     return (
         <Swiper
           style={styles.wrapper}
           dot={
-            <View style={styles.Dot}/>
+            <View
+              style={{
+                backgroundColor: 'rgba(255,255,255,.3)',
+                width: 10,
+                height: 10,
+                borderRadius: 7,
+                marginLeft: 7,
+                marginRight: 7
+              }}
+            />
           }
           activeDot={
-            <View style={styles.ActiveDot}/>
+            <View
+              style={{
+                backgroundColor: '#fff',
+                width: 10,
+                height: 10,
+                borderRadius: 7,
+                marginLeft: 7,
+                marginRight: 7
+              }}
+            />
           }
           paginationStyle={{
             bottom: 70
           }}
           loop={false}
         >
-		  
+
 		  <View style={styles.slide1}>
-		   <HomeScreen parentCallback = {this.setName}/>
-          </View>
+			<WeatherScreen/>
+		  </View>
 		  
 		  <View style={styles.slide2}>
-		    <GeolocationExample Stored_name = {this.state.stored_name} Stored_time = {this.state.stored_time}/>
+			<MusicScreen/>
+		  </View>
+		  
+		  <View style={styles.slide3}>
+			<SearchScreen parentCallback = {this.setName}/>
+		    <Text style = {styles.UserText}>Search Result: {this.state.stored_name}</Text>
+		    <Text style = {styles.BottomText}>Created by Xingjian Wang</Text>
 		  </View>
 		  
         </Swiper>
@@ -320,3 +339,6 @@ export default class APP extends Component {
     )
   }
 }
+
+
+
