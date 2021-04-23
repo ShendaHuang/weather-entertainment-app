d2a4d3af5c488227db83e62752a7a189ab187670
Xingjian Wang <xwang7@villanova.edu>
2021-04-23 19:09:18 +0800

    Create Music.js
    Add play and pause music functionalities to the music pages. Play and pause icons are also added.

---
+++ app/screens/Music.js
@@ -0,0 +1,163 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView, ScrollView, Dimensions, Alert } from 'react-native';
+import { Audio } from 'expo-av';
+import {FontAwesome5, AntDesign} from '@expo/vector-icons';
+
+const { height } = Dimensions.get('window');
+
+export class Music extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search_weather: '',
+            music_list: '',
+            screenHeight: 0,
+        }
+    }
+	
+  Sound = async() => {
+    console.log('Loading Sound')
+	
+	this.sound = new Audio.Sound();
+	
+    this.sound.loadAsync(
+       require('../../assets/bumblebee-sound.mp3')
+    );
+ 
+	let { positionMillis } = await this.sound.getStatusAsync()
+	}
+	
+  playSound = async() => {
+	console.log('Playing Sound');
+	let { positionMillis } = await this.sound.getStatusAsync()
+	if(positionMillis == 0) { await this.sound.playAsync();}
+    else {await this.sound.playFromPositionAsync(positionMillis)}
+	}
+	
+
+  pauseSound = async() => {
+	  
+    await this.sound.pauseAsync(); 
+	let { isLoaded, isPlaying, positionMillis } = await this.sound.getStatusAsync()
+	console.log(positionMillis)
+	await this.sound.setStatusAsync({positionMillis:positionMillis})
+  }
+
+    onContentSizeChange = (contentWidth, contentHeight) =>{
+        this.setState({screenHeight: contentHeight});
+    }
+	
+    getMusicList = (weather) =>{
+        const music_lists = require("../../assets/songWeather.json");
+        // this.setState({music_list: music_lists["Sunny"]})
+        this.setState({music_list: music_lists[weather]})
+    }
+	
+	_onPressButton() {
+	Alert.alert('Name Saved!')
+	}
+    
+    render() {
+        
+        var music_list_display = [];
+        for(var i in this.state.music_list){
+            var text = (
+            <View key={i} style={styles.list_item}>
+				<TouchableOpacity style={styles.text_icon}
+					onPress={() => {this.Sound()}}>
+                <Text margin = {200}
+                    // onPress={this.show.bind(this, this.props.music_list_display[i])}
+                    numberOfLines={1}>
+                    {this.state.music_list[i]}
+                </Text>
+				</TouchableOpacity>
+				<FontAwesome5 
+					onPress={() => {this.playSound()}} name ="play" size={22} />
+				<AntDesign
+					onPress={() => {this.pauseSound()}} name ="pause" size={22} />
+            </View>
+            );
+            music_list_display.push(text);
+        }
+
+        const scrollEnabled = this.state.screenHeight > height;
+        return (
+            <SafeAreaView style={styles.container}>
+                <ScrollView 
+                    style={{ flex: 1 }}
+                    contentContainerStyle={styles.scrollview}
+                    scrollEnabled={scrollEnabled}
+                    onContentSizeChange={this.onContentSizeChange}
+                >
+                    <Text>Music Screen for weather: {this.props.searchTerm}</Text>
+
+                    <TouchableOpacity style={styles.searchMusicButton}
+                        onPress={() => {this.getMusicList(this.props.searchTerm)}}> 
+                        <Text style={styles.searchButtonText}>Search Music</Text>
+                    </TouchableOpacity>
+
+                    { music_list_display }
+
+                </ScrollView>
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        paddingTop:60,
+        backgroundColor: '#b3dbff',
+    },
+    scrollview:{
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop:60,
+        backgroundColor: '#b3dbff',
+    },
+    searchMusicButton:{
+        borderWidth: 1,
+        borderColor: '#007BFF',
+        borderRadius: 10,
+        backgroundColor: '#007BFF',
+        width:200,
+        padding: 15,
+        margin: 5,
+        marginBottom: 30
+    },
+    searchButtonText: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    list_item:{
+        marginLeft:5,
+        marginRight:5,
+        padding:5,
+        borderWidth:1,
+        height:40,
+		flex:1,
+		flexDirection:'row',
+        borderRadius:3,
+        borderColor:'#ddd'
+    },
+	text_icon:{
+		marginLeft:5,
+        marginRight:5,
+        padding:5,
+        //borderWidth:1,
+        height: 30,
+		width:100,
+		flex:1,
+		justifyContent:'flex-start',
+		flexDirection:'row',
+        borderRadius:3,
+        borderColor:'#ddd'
+	},
+});
+
+export default Music;
